cb06349b006b196a8cf621a77ba670a8fcd579ce
chongxi <chongxi.lai@gmail.com>
2018-06-05 09:30:45 -0400

    prb.connect

--- playground/view/ephys_view.py
+++ playground/view/ephys_view.py
@@ -2,10 +2,13 @@ import numpy as np
 from spiketag.probe import prb_bowtie_L as prb 
 from spiketag.view import probe_view
 
+@prb.connect
+def on_select(group_id, chs):
+    print(group_id, chs)
 
 if __name__ == '__main__':
     print(prb)
+    prb.show()
     # prb_view = probe_view()
     # prb_view.set_data(prb, font_size=17)
     # prb_view.run()
-    prb.show()
