06099124c80cc076cf5587932f7f18d8fec61557
chongxi lai <chongxi.lai@gmail.com>
2018-09-18 17:43:37 -0400

    simple/complex maze folder

---
+++ playground/base/maze/blender_conversion/convert_mesh_obj.py
@@ -0,0 +1,33 @@
+
+
+filename = "simple_maze.obj"
+ofilename = 'tri_' + filename 
+
+f = open(filename)
+ofile = open(ofilename, "w")
+
+line = "#SHOULD NOT BE HERE THIS LINE IS"
+while line:
+    line = f.readline()
+    if len(line) == 0: continue
+    if line[0] == "f":
+        # if the number of entries following the f is more than 3,
+        # write multiple face lines as all triangles
+        faces = []
+        oldindex = 1
+        index = line.find(" ", 2)
+        while index != -1:
+            faces.append(line[oldindex+1:index])
+            oldindex = index
+            index = line.find(" ", index+1)
+        # I'm assuming no faces at end of line
+        faces.append(line[oldindex+1:len(line)-1])
+        #print faces
+        start = 1
+        while start + 1 < len(faces):
+            ofile.write("f " + faces[0] + " " + faces[start] + " " + faces[start+1] + "\n")
+            start += 1
+    else:
+        ofile.write(line)
+ofile.close()
+f.close()
