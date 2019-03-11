5ccf294f41b6fb09d71f7d56a22379bd4a636580
chongxi <chongxi.lai@gmail.com>
2019-03-11 17:38:34 -0400

    python 3 load npy needs set encoding='bytes'

--- tests/place_cell/test_replay.py
+++ tests/place_cell/test_replay.py
@@ -10,14 +10,15 @@ from playground.view import maze_view
 
 if __name__ == '__main__':
     nav_view = maze_view()
-    nav_view.load_maze('../playground/base/maze/obj/maze_2d.obj', '../playground/base/maze/obj/maze_2d.coords')
+    nav_view.load_maze('/disk0/Work/pydev/playground/playground/base/maze/complex/maze_2d.obj',
+                       '/disk0/Work/pydev/playground/playground/base/maze/complex/maze_2d.coords')
     # nav_view.load_cue(cue_file='../playground/base/maze/obj/constraint_cue.obj', cue_name='_dcue_001')
     # nav_view.load_cue(cue_file='../playground/base/maze/obj/goal_cue.obj', cue_name='_dcue_000')
     nav_view.load_replay_file('./task_replay.npz', show=False)
     nav_view.load_neurons('./timing.npz', 'timing')
     nav_view.replay_speed = 10
     nav_view.replay_time = 10
-    nav_view.neuron_id = 3
+    nav_view.neuron_id = [3,5,6]
     nav_view.replay_timer.start(0.01)
     nav_view.run()
     
